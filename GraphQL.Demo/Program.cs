fd7603e9006099a13dbdbc4afdb2ae82086881fa
Ismae Miah <ismail27.dec@gmail.com>
2023-04-02 11:17:11 +0600

    GraphQL implemented

--- GraphQL.Demo/Program.cs
+++ GraphQL.Demo/Program.cs
@@ -29,6 +29,19 @@ app.Run();
 
 
 //Implement GraphQL
-public class LibraryQuery{
+public record Book(string Title, Author Author);
+public record Author(string Name);
+public class LibraryQuery
+{
+    readonly List<Book?> _book = new(){
+        new Book("I Love GraphQL", new Author("Brandon Minnick")),
+        new Book("GraphQL is the Future", new Author("Brandon Minnick")),
+        new Book("I Love SOAP + XML", new Author("John 'I Hate New Technology'")),
+        null
+    };
 
+    public List<Book?> GetBooks() => _book;
+    public Book? GetBook(string title) => _book.FirstOrDefault(x => x?.Title == title);
+
+    public Author? GetAuthor(string name) => _book.Where(x => x?.Author.Name == name).FirstOrDefault()?.Author;
 }
