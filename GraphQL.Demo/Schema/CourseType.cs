fea720dff4cc4111a3c3221cebab10a2e46ff36c
Ismae Miah <ismail27.dec@gmail.com>
2023-04-02 14:35:13 +0600

    Modified Course type

--- GraphQL.Demo/Schema/CourseType.cs
+++ GraphQL.Demo/Schema/CourseType.cs
@@ -1,3 +1,10 @@
+
+public enum Subject{
+    Mathematics,
+    Science,
+    History
+}
+
 public class CourseType
 {
     public Guid Id { get; set; }
@@ -5,12 +12,5 @@ public class CourseType
     public Subject Subject { get; set; }
     [GraphQLNonNullType]
     public InstructorType Instructor { get; set; }
-    [GraphQLNonNullType]
     public IEnumerable<StudentType> Students { get; set; }
-}
-
-public enum Subject{
-    Mathematics,
-    Science,
-    History
 }
