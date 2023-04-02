9c742749b83642813837ab0ccb90b0f557ec5a67
Ismae Miah <ismail27.dec@gmail.com>
2023-04-02 11:34:04 +0600

    Set Deprecated Query

--- GraphQL.Demo/Schema/LibraryQuery.cs
+++ GraphQL.Demo/Schema/LibraryQuery.cs
@@ -10,8 +10,12 @@ public class LibraryQuery
         null
     };
 
+    [GraphQLDeprecated("This query is deprecated.")]
     public List<Book?> GetBooks() => _book;
+    
+    [GraphQLDeprecated("This query is deprecated.")]
     public Book? GetBook(string title) => _book.FirstOrDefault(x => x?.Title == title);
 
+    [GraphQLDeprecated("This query is deprecated.")]
     public Author? GetAuthor(string name) => _book.Where(x => x?.Author.Name == name).FirstOrDefault()?.Author;
 }
