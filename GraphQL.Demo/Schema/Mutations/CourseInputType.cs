08b38cff061c4848140877703eea6183230ad85d
Ismae Miah <ismail27.dec@gmail.com>
2023-04-03 15:05:19 +0600

    Implement courseInputType for Mutation

---
+++ GraphQL.Demo/Schema/Mutations/CourseInputType.cs
@@ -0,0 +1,6 @@
+public class CourseInputType
+{
+    public string Name { get; set; }
+    public Subject Subject { get; set; }
+    public Guid InstructorId { get; set; }
+}
--- GraphQL.Demo/Schema/Mutations/Mutation.cs
+++ GraphQL.Demo/Schema/Mutations/Mutation.cs
@@ -8,14 +8,14 @@ public class Mutation
         _courses = new List<CourseResult>();
     }
 
-    public CourseResult CreateCourse(string name, Subject subject, Guid instructorId)
+    public CourseResult CreateCourse(CourseInputType courseInput)
     {
         CourseResult courseResult = new CourseResult
         {
             Id = Guid.NewGuid(),
-            Name = name,
-            Subject = subject,
-            InstructorId = instructorId
+            Name = courseInput.Name,
+            Subject = courseInput.Subject,
+            InstructorId = courseInput.InstructorId
         };
 
         _courses.Add(courseResult);
@@ -23,7 +23,7 @@ public class Mutation
         return courseResult;
     }
 
-    public CourseResult UpdateCourse(Guid id, string name, Subject subject, Guid instructorId)
+    public CourseResult UpdateCourse(Guid id, CourseInputType courseInput)
     {
         CourseResult course = _courses.FirstOrDefault(c => c.Id == id);
 
@@ -32,9 +32,9 @@ public class Mutation
             throw new GraphQLException(new Error("Course not found.", "COURSE_NOT_FOUND"));
             //throw new Exception("Course not found");
         }
-        course.Name = name;
-        course.Subject = subject;
-        course.InstructorId = instructorId;
+        course.Name = courseInput.Name;
+        course.Subject = courseInput.Subject;
+        course.InstructorId = courseInput.InstructorId;
 
         return course;
     }
