10a8bdc7060b99ade8c32ff315e0a9c62c3aae9d
Ismael Miah <ismail27.dec@gmail.com>
2023-04-18 01:13:30 +0600

    Apply creatorId with migration

--- GraphQL.Demo/Dtos/CourseDto.cs
+++ GraphQL.Demo/Dtos/CourseDto.cs
@@ -3,6 +3,7 @@ public class CourseDto
     public Guid Id { get; set; }
     public string Name { get; set; }
     public Subject Subject { get; set; }
+    public string CreatorId { get; set; }
     public Guid InstructorId { get; set; }
     public InstructorDto Instructor { get; set; }
     public IEnumerable<StudentDto> Students { get; set; }
---
+++ GraphQL.Demo/Migrations/20230417183554_CreatorId added.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQL.Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatorIdadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorId",
+                table: "Courses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "Courses");
+        }
+    }
+}
